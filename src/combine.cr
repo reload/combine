d20ac7e9d65ee62886982907ae1d00f942ea17c3
Thomas Fini Hansen <xen@xen.dk>
2024-06-12 20:13:39 +0200

    Only sync every five minutes

--- src/combine.cr
+++ src/combine.cr
@@ -1,14 +1,19 @@
 require "../config/application"
 
-CronScheduler.define do
-  at("* * * * *") {
-    harvest = Harvest.new(
-      Amber.settings.secrets["harvest_account_id"],
-      Amber.settings.secrets["harvest_token"],
-    )
+def sync
+  harvest = Harvest.new(
+    Amber.settings.secrets["harvest_account_id"],
+    Amber.settings.secrets["harvest_token"],
+  )
 
-    Sync.new(harvest).run
-  }
+  Sync.new(harvest).run
+end
+
+# Run cron tasks in the master process.
+if Amber::Cluster.master?
+  CronScheduler.define do
+    at("5 * * * *") { sync }
+  end
 end
 
 Amber::Support::ClientReload.new if Amber.settings.auto_reload?
